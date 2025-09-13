37aa7d74351d35c494de7b5cd68c386b28a02ba0
Ramesh Sundharam <rameshsgbm456@gmail.com>
2025-09-13 20:36:58 +0530

    Adding Primeng and linkedSignal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import  Aura  from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+     providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ]
 };
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, httpResource } from '@angular/common/http';
-import { Component, OnInit, resource, signal, Signal } from '@angular/core';
+import { Component, linkedSignal, OnInit, resource, signal, Signal } from '@angular/core';
+import { Button, ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonModule,CardModule,SkeletonModule,CarouselModule,TagModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
@@ -12,6 +18,28 @@ export class Home implements OnInit {
   ngOnInit(): void {
    console.log("Home component initialized");
   }
+   responsiveOptions = [
+            {
+                breakpoint: '1400px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '1199px',
+                numVisible: 3,
+                numScroll: 1
+            },
+            {
+                breakpoint: '767px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '575px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ]
   userDatas = resource({
     loader: async () => {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -28,6 +56,26 @@ export class Home implements OnInit {
   //     return await response.json();
   //   }
   // })
-  selectedUserData = httpResource<any>(() => this.selectedUser() === 0 ? undefined : `https://jsonplaceholder.typicode.com/users/${this.selectedUser()}`);
+  selectedUserData = httpResource<any[]>(() => this.selectedUser() === 0 ? undefined : `https://jsonplaceholder.typicode.com/albums?userId=${this.selectedUser()}`);
 
+   getSeverity(status: string) {
+        switch (status) {
+            case 'INSTOCK':
+                return 'success';
+            case 'LOWSTOCK':
+                return 'warn';
+            case 'OUTOFSTOCK':
+                return 'danger';
+                default:
+                return 'info';
+        }
+    }
+
+    selectedUserAlbum = linkedSignal({
+      source: this.selectedUser,
+      computation: () => {  
+        return 0;
+      }
+    });
+    selectedUserAlbumData = httpResource<any[]>(() => this.selectedUserAlbum() === 0 ? undefined : `https://jsonplaceholder.typicode.com/photos?albumId=${this.selectedUserAlbum()}`);
 }
